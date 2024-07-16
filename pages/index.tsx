d90e2ef15011d4be0d6512731eda591d244de5a3
Chavez Harris <codedbychavez@gmail.com>
2024-07-16 07:09:09 -0400

    add types and improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,44 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import * as configcat from "configcat-js-ssr";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+import type { ApiResponse } from '@/pages/types/ApiResponse'
+
+export const getServerSideProps = (async () => {
+  // Create the ConfigCat client with your SDK Key
+  const configCatClient = configcat.getClient("YOUR-SDK-KEY");
+
+  // Get your setting value
+  const randomUserFeature = await configCatClient.getValueAsync(
+    "randomUserFeature",
+    false
+  );
+
+  // Get a random user
+  const res = await fetch("https://randomuser.me/api/");
+  const data = await res.json();
+
+  return {
+    props: {
+      randomUser: data.results[0],
+      randomUserFeature,
+    },
+  };
+}) satisfies GetServerSideProps<{
+  randomUser: any;
+  randomUserFeature: boolean;
+}>;
+
+export default function Home({
+  randomUser,
+  randomUserFeature,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  // Extract the required user details
+  // const firstName = 
+
   return (
     <>
       <Head>
@@ -15,99 +50,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <div>Welcome to main</div>
       </main>
     </>
   );
